47e2e7883f34e5798ff0cfe051d235e33a21217d
danicroos <daniol2700@gmail.com>
2023-09-11 11:49:17 -0600

    Calcular Reales

--- calculadora.py
+++ calculadora.py
@@ -7,8 +7,17 @@ Descripcion: Calculadora que sume
 a = input("Ingresa el primer valor ")
 b = input("Ingresa el segundo valor ")
 print(type(a), type(b))
-print("suma ",a + b)
+print("Suma ",a + b)
 a = float(a)
 b = float(b)
 print(type(a), type(b))
-print("suma ",a + b)
+print("Suma ",a + b)
+
+print("Valor de la resta ")
+print("Resta ",a - b)
+
+print("Valor de la Multiplicacion ")
+print("Multiplicar ",a * b)
+
+print("Valor de la Divicion ")
+print("Dividir ",a / b)
