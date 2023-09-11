d24b84d4cc53a18bcb10f1734b9a9369715172b0
danicroos <daniol2700@gmail.com>
2023-09-11 11:36:08 -0600

    Sumar Reales

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+'''
+Autor: Daniel Zuñiga
+Fecha: 11-Sep-2023
+Descripcion: Calculadora que sume 
+'''
+
+a = input("Ingresa el primer valor ")
+b = input("Ingresa el segundo valor ")
+print(type(a), type(b))
+print("suma ",a + b)
+a = float(a)
+b = float(b)
+print(type(a), type(b))
+print("suma ",a + b)
