98501e1593cc3797a02effc316951fbdfbb027d8
danicroos <142865312+danicroos@users.noreply.github.com>
2023-10-09 23:53:46 -0600

    LAB_4.1.1.1

---
+++ Modulo.4/LAB_4.3.1.10.py
@@ -0,0 +1,21 @@
+'''
+Autor:  Zuñiga Olvera Jorge Daniel
+Fecha:  09-oct-2023
+Descripcion: 
+'''
+def liters_100km_to_miles_gallon(liters):
+
+    miles = 100 / ((liters / 3.785411784) * 1.609344)
+    return miles
+
+def miles_gallon_to_liters_100km(miles):
+
+    liters = (100 / miles) * (3.785411784 / 1.609344)
+    return liters
+
+print(liters_100km_to_miles_gallon(3.9))
+print(liters_100km_to_miles_gallon(7.5))
+print(liters_100km_to_miles_gallon(10.))
+print(miles_gallon_to_liters_100km(60.3))
+print(miles_gallon_to_liters_100km(31.4))
+print(miles_gallon_to_liters_100km(23.5))
---
+++ Modulo.4/LAB_4.3.1.9.py
@@ -0,0 +1,19 @@
+'''
+Autor:  Zuñiga Olvera Jorge Daniel
+Fecha:  09-oct-2023
+Descripcion: 
+'''
+import math
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+for i in range(1, 20):
+	if is_prime(i + 1):
+			print(i + 1, end=" ")
+print()
